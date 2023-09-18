465d0048ef827397a11199cd118d4b4dc5b92a9b
Mike Stromme <mstromme@suburbanenterprises.com>
2023-09-18 11:15:49 -0500

    x

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, jsonify
 import time
+import subprocess
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 # Simulated progress
 current_progress = 0
+
+def run_demucs():
+    process = subprocess.Popen(['demucs', '-n', 'mdx_extra', 'input/Barracuda.mp3'], 
+                               stdout=subprocess.PIPE, 
+                               stderr=subprocess.STDOUT, 
+                               universal_newlines=True)
+
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            # Parse the output to get progress information
+            # Send progress back to the browser
+            progress = subprocess.run(['demucs', '-n', 'mdx_extra', 'input/Barracuda.mp3'])  # Extract progress from output  # Extract progress from output
+            print(progress)  # For testing, you can print it out
+            socketio.emit('update_progress', {'progress': progress})
+
 
 @app.route('/')
 def index():
@@ -16,6 +37,7 @@ def get_progress():
 
     if current_progress < 100:
         current_progress += 1
+        #current_progress = demucs -n mdx_extra "You Really Got Me.mp3"
 
     return jsonify({'progress': current_progress})
 
